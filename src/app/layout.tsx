aa4b1e3364f91603491b0e0c1f3f47ef7e1a5496
TKSora <tksorabr@gmail.com>
2024-01-28 22:57:15 -0300

    Bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from 'next'
-import { Inter as FontSans } from 'next/font/google'
+
 import './globals.css'
 import { cn } from '@/libs/utils'
 import DefaultLayout from '@/components/layouts/default-layout'
 import AuthProvider from '@/contexts/AuthProvider'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '../libs/auth'
+import { fontSans } from '@/libs/fonts'
 
-export const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans',
-})
 export const metadata: Metadata = {
   title: 'twitch.tv/dezzasz',
   description: 'Live em twitch.tv/dezzasz',
